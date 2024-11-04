ded177697f88b335511c79bba55c8debcf1190f1
Abdo-Ahmed-Mostafa <abdoahmedd20240@gmail.com>
2024-11-04 15:47:08 +0200

    update error ahmed asker

--- components/ClientLayout.jsx
+++ components/ClientLayout.jsx
@@ -1,14 +1,15 @@
 "use client";
-import { ThemeProvider } from "@/clientSide";
+import { ThemeProvider } from "@material-tailwind/react";
 import HomeNav from "./mainComponents/navbar/HomeNav";
-
 
 const ClientLayout = ({ children }) => {
   return (
-    <ThemeProvider>
-      <HomeNav />
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <HomeNav />
+        {children}
+      </ThemeProvider>
+    </>
   );
 };
 
